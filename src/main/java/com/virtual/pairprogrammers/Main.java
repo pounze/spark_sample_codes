58b291bd058948514839ccd7be39ce1a21ef4fbf
Sudeep Dasgupta <sudeep.ignition@gmail.com>
2019-12-17 23:25:39 +0530

    DStreams + kafka basic commands

--- src/main/java/com/virtual/pairprogrammers/Main.java
+++ src/main/java/com/virtual/pairprogrammers/Main.java
@@ -14,6 +14,7 @@ import org.apache.spark.sql.types.*;
 import org.apache.spark.streaming.Duration;
 import org.apache.spark.streaming.Durations;
 import org.apache.spark.streaming.api.java.JavaDStream;
+import org.apache.spark.streaming.api.java.JavaPairDStream;
 import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import scala.Tuple2;
@@ -42,9 +43,15 @@ public class Main
 
         JavaReceiverInputDStream<String> inputData = ssc.socketTextStream("localhost", 8989);
 
-        JavaDStream<Object> results = inputData.map(item -> item);
+        JavaDStream<String> results = inputData.map(item -> item);
 
-        results.print();
+//        results = results.map(rawLogMessage -> rawLogMessage.split(",")[0]);
+
+        JavaPairDStream<String, Long> pairDStream = results.mapToPair(rawLogMessage -> new Tuple2<>(rawLogMessage.split(",")[0],1L));
+
+        pairDStream = pairDStream.reduceByKey((x,y) -> x+y);
+
+        pairDStream.print();
 
         ssc.start();
 
